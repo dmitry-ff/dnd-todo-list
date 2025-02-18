873c7dc9b10eb6acd286ab264c34235be3aaf121
dmitry <dima.kozlov.2186@gmail.com>
2025-02-18 14:53:45 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {KeyboardEventHandler, useState} from 'react'
+import * as React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<string[]>([])
+
+  const handleTodoAdd: KeyboardEventHandler<HTMLInputElement>  =  (e) => {
+    console.log(e)
+    if (e.key === "Enter") {
+      console.log("hello")
+      setTodos((t) => [...t, e.currentTarget])
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <ul>
+        {todos.map((todo) => <li>{todo}</li>)}
+      </ul>
+      <input className="border-indigo-600 border-2 outline-0" onKeyDown={handleTodoAdd}/>
+    </div>
   )
 }
 
